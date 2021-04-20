8ad7f03d1cda2738dc2c169b40eb6e3d891fbd96
Eric <deleoneric97@gmail.com>
2021-04-19 21:25:55 -0400

    More Fixes

--- ML-stock-prediction.py
+++ ML-stock-prediction.py
@@ -241,12 +241,10 @@ def results_prediction(pred, score):
     st.write(results_df_transposed)
 
 def regression_results(y_true, y_pred):
-    explained_variance=metrics.explained_variance_score(y_true, y_pred)
     mean_absolute_error=metrics.mean_absolute_error(y_true, y_pred) 
     mse=metrics.mean_squared_error(y_true, y_pred) 
     mean_squared_log_error=metrics.mean_squared_log_error(y_true, y_pred)
-    r2=metrics.r2_score(y_true, y_pred)
-    print('explained_variance: ', round(explained_variance,4))    
+    r2=metrics.r2_score(y_true, y_pred)  
     print('mean_squared_log_error: ', round(mean_squared_log_error,4))
     print('r2: ', round(r2,4))
     print('MAE: ', round(mean_absolute_error,4))
